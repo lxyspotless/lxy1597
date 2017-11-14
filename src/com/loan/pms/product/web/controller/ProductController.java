4c631ebf94bab2ec7913ac612ab2d517d8ec4024
yfu <yfu@WIN-FY>
2017-11-14 22:47:35 +0800

    ok

--- src/com/loan/pms/product/web/controller/ProductController.java
+++ src/com/loan/pms/product/web/controller/ProductController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.loan.pms.product.dto.ProductInfoDTO;
 import com.loan.pms.product.service.ProductService;
 import com.loan.pms.system.web.controller.SystemController;
