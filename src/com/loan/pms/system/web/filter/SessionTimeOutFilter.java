3789263d614e600226a6db0ed5ebe633f69e6f3a
yfu <yfu@WIN-FY>
2018-01-11 00:22:46 +0800

    OK

--- src/com/loan/pms/system/web/filter/SessionTimeOutFilter.java
+++ src/com/loan/pms/system/web/filter/SessionTimeOutFilter.java
@@ -18,25 +18,29 @@ public class SessionTimeOutFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		Long lastActiveTime = (Long) request.getSession().getAttribute(SystemFinal.LAST_ACTIVE_TIME);
-		// 未登录无需判断是否超时
-		if(null != lastActiveTime){
-			String loginOutTimeMin = this.getFilterConfig().getInitParameter("loginTimeOutMin");
-			if(!StringUtils.isEmpty(loginOutTimeMin)){
-				if(lastActiveTime + Long.parseLong(loginOutTimeMin)*60*1000 < System.currentTimeMillis()){
-					// 超时，清空session并跳转登录页面
-					request.getSession().removeAttribute(SystemFinal.LOGIN_USERNAME);
-					request.getSession().removeAttribute(SystemFinal.LAST_ACTIVE_TIME);
-					request.getSession().setAttribute(SystemFinal.IS_SESSION_TIME_OUT, SystemFinal.YES);
-					PrintWriter writer = response.getWriter();
-					writer.print("{sessionOutOfTime:0}");
-					return;
-				}else{
-					// 未超时，将本次动作时间更新写入session
-					request.getSession().setAttribute(SystemFinal.LAST_ACTIVE_TIME, System.currentTimeMillis());
+		String loginTimeOutSwitch = this.getFilterConfig().getInitParameter("loginTimeOutSwitch");
+		// 登录超时开关已打开
+		if("1".equals(loginTimeOutSwitch)) {
+			Long lastActiveTime = (Long) request.getSession().getAttribute(SystemFinal.LAST_ACTIVE_TIME);
+			// 未登录无需判断是否超时
+			if(null != lastActiveTime) {
+				String loginOutTimeMin = this.getFilterConfig().getInitParameter("loginTimeOutMin");
+				if(!StringUtils.isEmpty(loginOutTimeMin)) {
+					if(lastActiveTime + Long.parseLong(loginOutTimeMin)*60*1000 < System.currentTimeMillis()) {
+						// 超时，清空session并跳转登录页面
+						request.getSession().removeAttribute(SystemFinal.LOGIN_USERNAME);
+						request.getSession().removeAttribute(SystemFinal.LAST_ACTIVE_TIME);
+						request.getSession().setAttribute(SystemFinal.IS_SESSION_TIME_OUT, SystemFinal.YES);
+						PrintWriter writer = response.getWriter();
+						writer.print("{sessionOutOfTime:0}");
+						return;
+					} else {
+						// 未超时，将本次动作时间更新写入session
+						request.getSession().setAttribute(SystemFinal.LAST_ACTIVE_TIME, System.currentTimeMillis());
+					}
+				} else {
+					logger.error("获取过滤器初始:登录超时时间,为空！");
 				}
-			}else{
-				logger.error("获取过滤器初始:登录超时时间,为空！");
 			}
 		}
 		filterChain.doFilter(request, response);
