6cf64fddd5ac4c2034115f836089c30b0ea5945c
yfu <yfu@WIN-FY>
2018-01-23 00:00:14 +0800

    OK

--- src/com/loan/pms/test/ProductServiceTest.java
+++ src/com/loan/pms/test/ProductServiceTest.java
@@ -18,7 +18,7 @@ public class ProductServiceTest extends BaseSpringJunit4RunnerTest {
 	
 	@Test   //标明是测试方法  
 	@Transactional   //标明此方法需使用事务  
-	@Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
+	@Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚,默认回滚
 	public void queryAllSaleProductListTest() {
 		List<ProductInfoDTO> list = productDao.queryAllSaleProductList();
 		System.out.println(list.size());
--- src/com/loan/pms/test/base/BaseSpringJunit4RunnerTest.java
+++ src/com/loan/pms/test/base/BaseSpringJunit4RunnerTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(BaseSpringJUnit4Runner.class)  //使用junit4进行测试  
-@ContextConfiguration(locations={"classpath:config/applicationContext.xml"}) //加载配置文件  
+@ContextConfiguration(locations={"classpath:core/applicationContext.xml"}) //加载配置文件  
 //如果加入以下代码，所有继承该类的测试类都会遵循该配置，也可以不加，在测试类的方法上控制事务
 //这个非常关键，如果不加入这个注解配置，事务控制就会完全失效！  
 //这里的事务关联到配置文件中的事务控制器（transactionManager = "transactionManager"），同时//指定自动回滚（defaultRollback = true）。这样做操作的数据才不会污染数据库！  
