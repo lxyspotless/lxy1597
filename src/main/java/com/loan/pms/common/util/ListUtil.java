17c7728b779ccddf5621c64d29d5834b59b65e8f
yfu <yfu@DESKTOP-OCRUJ8N>
2018-05-26 22:06:36 +0800

    OK

--- src/main/java/com/loan/pms/common/util/ListUtil.java
+++ src/main/java/com/loan/pms/common/util/ListUtil.java
@@ -3,11 +3,29 @@ package com.loan.pms.common.util;
 import java.util.List;
 
 public class ListUtil {
+	/**
+	 * 判断List是否为空，为空则返回true
+	 * @param list
+	 * @return boolean
+	 */
 	public static <T> boolean isEmpty(List<T> list){
+		boolean resultFlag = true;
 		if(null != list && list.size() > 0){
-			return false;
-		}else{
-			return true;
+			resultFlag = false;
 		}
+		return resultFlag;
+	}
+	
+	/**
+	 * 判断List是否不为空，不为空则返回true
+	 * @param list
+	 * @return boolean
+	 */
+	public static <T> boolean isNotEmpty(List<T> list){
+		boolean resultFlag = false;
+		if(null != list && list.size() > 0){
+			resultFlag = true;
+		}
+		return resultFlag;
 	}
 }
